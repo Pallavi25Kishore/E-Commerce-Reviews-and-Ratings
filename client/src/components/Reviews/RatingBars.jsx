dd4d1dca3c2040fe701e82d1f95b8478acec2e9c
Pallavi <pallavi25kishore@gmail.com>
2024-06-11 16:18:34 -0700

    implement rating progress highlight on hovering feature

--- client/src/components/Reviews/RatingBars.jsx
+++ client/src/components/Reviews/RatingBars.jsx
@@ -4,38 +4,38 @@ const RatingBars = ({ratings, totalNumberOfRatings}) => {
 
   const numOfFiveStars = Number(ratings['5']);
   const Five = (numOfFiveStars / totalNumberOfRatings) * 100;
-  console.log(One);
+
 
   const numOfFourStars = Number(ratings['4']);
   const Four = (numOfFourStars / totalNumberOfRatings) * 100;
-  console.log(Two);
+
 
   const numOfThreeStars = Number(ratings['3']);
   const Three = (numOfThreeStars / totalNumberOfRatings) * 100;
-  console.log(Three);
+
 
   const numOfTwoStars = Number(ratings['2']);
   const Two = (numOfTwoStars / totalNumberOfRatings) * 100;
-  console.log(Four);
 
-    const numOfOneStars = Number(ratings['1']);
-    const One = (numOfOneStars / totalNumberOfRatings) * 100;
-    console.log(Five);
+
+  const numOfOneStars = Number(ratings['1']);
+  const One = (numOfOneStars / totalNumberOfRatings) * 100;
+
 
   return (
     <div>
 
-      <div className="five">
-        <span>5 stars</span>
-        <div style={{position: 'relative', color: 'grey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
+      <div className="five" >
+        <span >5 stars</span>
+        <div  style={{position: 'relative', backgroundColor: 'lightgrey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
           <div style={{height: '100%', width: `${Five}%`, backgroundColor: 'green'}}></div>
         </div>
-        <span>{numOfFiveStars}</span>
+        <span >{numOfFiveStars}</span>
       </div>
 
       <div className="four">
         <span>4 stars</span>
-        <div style={{position: 'relative', color: 'grey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
+        <div style={{position: 'relative', backgroundColor: 'lightgrey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
           <div style={{height: '100%', width: `${Four}%`, backgroundColor: 'green'}}></div>
         </div>
         <span>{numOfFourStars}</span>
@@ -43,7 +43,7 @@ const RatingBars = ({ratings, totalNumberOfRatings}) => {
 
       <div className="three">
         <span>3 stars</span>
-        <div style={{position: 'relative', color: 'grey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
+        <div style={{position: 'relative', backgroundColor: 'lightgrey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
           <div style={{height: '100%', width: `${Three}%`, backgroundColor: 'green'}}></div>
         </div>
         <span>{numOfThreeStars}</span>
@@ -51,7 +51,7 @@ const RatingBars = ({ratings, totalNumberOfRatings}) => {
 
       <div className="two">
         <span>2 stars</span>
-        <div style={{position: 'relative', color: 'grey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
+        <div style={{position: 'relative', backgroundColor: 'lightgrey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
           <div style={{height: '100%', width: `${Two}%`, backgroundColor: 'green'}}></div>
         </div>
         <span>{numOfTwoStars}</span>
@@ -60,7 +60,7 @@ const RatingBars = ({ratings, totalNumberOfRatings}) => {
 
       <div className="one">
         <span>1 stars</span>
-        <div style={{position: 'relative', color: 'grey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
+        <div style={{position: 'relative', backgroundColor: 'lightgrey', height: '10px', width: '200px', border: '1px solid black', margin: '5px'}}>
           <div style={{height: '100%', width: `${One}%`, backgroundColor: 'green'}}></div>
         </div>
         <span>{numOfOneStars}</span>
