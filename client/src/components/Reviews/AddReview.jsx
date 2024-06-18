23293c1d70bcfa7cd8e0f167e06557ab9ecb8058
Pallavi <pallavi25kishore@gmail.com>
2024-06-18 13:35:06 -0700

    Add tests - line coverage is now above 70 percent for reviews component

--- client/src/components/Reviews/AddReview.jsx
+++ client/src/components/Reviews/AddReview.jsx
@@ -17,7 +17,7 @@ const AddReview = ({metaData}) => {
 
   return (
     <div className="add-review-button">
-    <button onClick={handleAddReviewClick}>Add a Review  &#43;</button>
+    <button data-testid="add-review-btn" onClick={handleAddReviewClick}>Add a Review  &#43;</button>
     {FormOpen ? <FormModal handleCloseForm={handleCloseForm} metaData={metaData}/> : null}
     </div>
   )
